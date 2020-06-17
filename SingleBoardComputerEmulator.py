c86c4535838863e11f3cc439fbfe333a8249a7c5
SyedRahibZakaria <syedrahibreda@gmail.com>
2020-06-17 07:23:44 +0300

    First commit

--- SingleBoardComputerEmulator.py
+++ SingleBoardComputerEmulator.py
@@ -5,7 +5,7 @@ import requests
 from keras.preprocessing import image
 
 
-image_path = "./test_images/Q.jpg"
+image_path = "./test_images/A.jpg"
 #image = tf.keras.preprocessing.image.load_img(image_path)
 img = image.img_to_array(image.load_img(image_path, target_size=(224, 224, 3))) / 255.
 img = np.expand_dims(img, axis = 0)
